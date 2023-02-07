cc405822e334cfba03858168144e4b90d3634c59
rahulsya <rahulsyaban@gmail.com>
2023-02-07 22:37:47 +0700

    chore:setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const App = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Tasksu app</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
