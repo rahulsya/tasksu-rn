c2ba30ed2829ab54339dd00987d70eeff92d0861
rahulsya <rahulsyaban@gmail.com>
2023-02-10 23:07:02 +0700

    feat:Add assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export {default as Backward} from './Backward.svg';
+export {default as Dashboard} from './dashboard.svg';
+export {default as Right} from './right.svg';
+export {default as Up} from './Up.svg';
+export {default as Project} from './Project.svg';
+export {default as User} from './user.svg';
