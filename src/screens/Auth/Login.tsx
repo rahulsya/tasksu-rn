25172fca3d1f5a517ffdb50b4a8edda3919a8d25
rahulsya <rahulsyaban@gmail.com>
2023-02-15 15:48:22 +0700

    feat : add auth screen

---
+++ src/screens/Auth/Login.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <SafeAreaView>
+      <Text>Login</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Login
